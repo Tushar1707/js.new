6c4f23107eb7a486d96b5f180a4a847be9f67d2d
tushar1707 <tushar07solanki@gmail.comm>
2024-10-22 23:59:21 +0530

    string.js

---
+++ 1_basics/string.js
@@ -0,0 +1,34 @@
+const name = "Neha"
+const  repoCount = 02
+
+// console.log(name + repoCount + " value");
+
+
+// console.log(`Hello my name is ${name} and my repoCount ${repoCount}`);
+
+const gameName = new String("abc")
+//console.log(gameName[2]);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+console.log(gameName.indexOf('b'));
+
+const NewStirng = gameName.substring(0,3)
+console.log(NewStirng);
+
+const anotherstring = gameName.slice(-2,3)
+console.log(anotherstring);
+
+const string  = "   tushar    "
+console.log(string);
+console.log(string.trim());
+
+
+const url = "https://jarvis.com/jarvis20AI"
+
+console.log(url.replace('20', '-'))
+
+console.log(url.includes('jarvis'));
+
+
