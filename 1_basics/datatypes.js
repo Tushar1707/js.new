8567622484e72aa6bcaa7ddbe91490ee87c5cd05
tushar1707 <tushar07solanki@gmail.comm>
2024-10-22 23:28:52 +0530

    datatypes

--- 1_basics/datatypes.js
+++ 1_basics/datatypes.js
@@ -14,3 +14,39 @@
 // object => array, function, object, date, regExp, set, map, weakMap
 
 
+// <------------Primitive datatypes---------->
+
+// 7 types : String , Number , Boolean , NULL , undefined, Symbol, Bigint
+// primitive datatypes are immutable
+
+
+
+
+// <---------------Refrence (Non primitive)------------------>
+
+// Array , Object , functions
+
+// <---------stack(primitive)---------Heap(Non-primitive)--------->
+// primitive data types are stored in stack
+
+let Myyoutube = "tushar"
+let anothermytube = "chai code"
+console.log(Myyoutube);
+console.log(anothermytube);
+
+let userone = {
+    name: "tushar",
+    age: 25,
+    occupation: "software engineer",
+}
+let usertwo = userone
+
+usertwo.age = 56
+console.log(userone.age);
+console.log(usertwo.age);
+
+
+
+
+
+
