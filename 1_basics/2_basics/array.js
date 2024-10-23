33d32d3ffc6f570de036a8e281b988e290b85523
tushar1707 <tushar07solanki@gmail.comm>
2024-10-24 01:52:19 +0530

    basic

---
+++ 1_basics/2_basics/array.js
@@ -0,0 +1,54 @@
+
+//const arr = [1,2,3,4,5,6]
+
+
+// arr.push(8)
+// console.log(arr);
+// arr.pop()
+// arr.unshift(9)
+// arr.shift()
+// console.log(arr);
+// const newArr = arr
+// console.log(arr);
+// console.log(typeof newArr.join());
+// console.log(typeof newArr);
+
+// Slice , Splice
+
+// const  myarr1 = arr.slice(1,3)
+// console.log("A",arr);
+
+// console.log(myarr1);
+// const myarr2 = arr.splice(1,3)
+// console.log("B",arr);
+
+// console.log( myarr2);
+
+const marvel_heros = ["ironman","Thor","Captain America" ,"Black Widow","Hulk","Spiderman"]
+const dc_heros =["flash", "superman"]
+//marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[2][1]);
+
+
+//  const allheros = marvel_heros.concat(dc_heros)
+// console.log(allheros);
+
+// const allnewheros = [...marvel_heros,...dc_heros]
+// console.log(allnewheros);
+
+// console.log(Array.isArray("Tushar"));
+// console.log(Array.from("Tushar"));
+// console.log(Array.from({name: "Neha "}));
+const score1 =100
+const score2 = 200
+const score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
