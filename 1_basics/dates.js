09da42cb1dfb18bc21b50e442573ecdf085dc8e9
tushar1707 <tushar07solanki@gmail.comm>
2024-10-23 15:15:34 +0530

    dates

---
+++ 1_basics/dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+console.log( typeof myDate);
+
+let myCreateDate = new Date(2023,0,23)
+console.log(myCreateDate.toString());
+
+
+
+
+
+
---
+++ 1_basics/numsmath.js
@@ -0,0 +1,44 @@
+// const score = 500
+// console.log(score);
+
+// const balance = new Number(700)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const othernum = 123.44444
+
+// console.log(othernum.toPrecision(4));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// <------------------Maths-------------------->
+
+console.log(Math);
+
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+
+console.log(Math.min(1,5,8,4,6));
+console.log(Math.max(1,5,8,4,6));
+
+console.log(Math.random()); // gives only value between 0 and 1
+console.log(Math.random()*10+1)
+ 
+const min = 10
+const max = 100
+
+console.log(Math.floor(Math.random()* (max-min))+ min);
+
+
+
+
+
+
+
